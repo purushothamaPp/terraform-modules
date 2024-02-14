71f0a976afc4fff026de8e83f3cc2de2f4f46b62
purushothama reddy <99128725+purushothamaPp@users.noreply.github.com>
2024-02-14 14:44:36 +0530

    Update main.tf

--- module/azure_vm/main.tf
+++ module/azure_vm/main.tf
@@ -1,52 +1,52 @@
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "RG" {
   name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "VNET" {
   name                = var.virtual_network_name
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.RG.location
+  resource_group_name = azurerm_resource_group.RG.name
 }
 
-resource "azurerm_subnet" "example" {
+resource "azurerm_subnet" "SUBNET" {
   name                 = var.subnet_name
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.RG.name
+  virtual_network_name = azurerm_virtual_network.VNET.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_public_ip" "example" {
+resource "azurerm_public_ip" "PUBIP" {
   name                = var.public_ip_name
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.RG.location
+  resource_group_name = azurerm_resource_group.RG.name
   allocation_method   = "Dynamic"
 }
 
-resource "azurerm_network_interface" "example" {
+resource "azurerm_network_interface" "NIC" {
   name                = var.network_interface_name
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.RG.location
+  resource_group_name = azurerm_resource_group.RG.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
+    subnet_id                     = azurerm_subnet.SUBNET.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.example.id
+    public_ip_address_id          = azurerm_public_ip.PUBIP.id
   }
 }
 
-resource "azurerm_linux_virtual_machine" "example" {
+resource "azurerm_linux_virtual_machine" "VM" {
   name                = var.virtual_machine_name
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.RG.name
+  location            = azurerm_resource_group.RG.location
   size                = "Standard_DS1_v2"
   admin_username      = var.admin_username
   admin_password      = var.admin_password
 
   network_interface_ids = [
-    azurerm_network_interface.example.id,
+    azurerm_network_interface.NIC.id,
   ]
 
   os_disk {
