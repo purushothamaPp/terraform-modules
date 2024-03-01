fc96d407c7c8be3201d129f5c63c5a31eac6973d
purushothama reddy <99128725+purushothamaPp@users.noreply.github.com>
2024-03-01 16:02:16 +0530

    Update main.tf

--- module/azure_vm/main.tf
+++ module/azure_vm/main.tf
@@ -44,7 +44,7 @@ resource "azurerm_linux_virtual_machine" "VM" {
   size                = "Standard_DS1_v2"
   admin_username      = var.admin_username
   admin_password      = var.admin_password
-  disable_password_authentication = false
+
 
   network_interface_ids = [
     azurerm_network_interface.NIC.id,
